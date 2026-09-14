9bcb4e01ae4bffdc43b20414c24e8cfe4ba10c19
Mostafa ELFAR <tatafar2018@gmail.com>
2026-09-14 16:43:31 +0300

    feat: Add API entry point and update Vercel configuration for backend

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,12 +2,18 @@ const mongoose = require("mongoose");
 const logger = require('../utils/Logger');
 
 async function connectdb() {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
   try {
+    if (!process.env.MONGO_URI) {
+      logger.error("MONGO_URI is missing in environment variables.");
+      return;
+    }
     await mongoose.connect(process.env.MONGO_URI);
     logger.info("MongoDB connected successfully");
   } catch (error) {
     logger.error("Error connecting to MongoDB:", error);
-    process.exit(1);
   }
 }
 
