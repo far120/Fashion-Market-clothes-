d61b51ff4694eb47a6482d712c44cfe61fb8c62c
Mostafa ELFAR <tatafar2018@gmail.com>
2026-09-14 16:47:04 +0300

    feat: Remove unused API entry point and Vercel configuration files

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,14 +2,7 @@ const mongoose = require("mongoose");
 const logger = require('../utils/Logger');
 
 async function connectdb() {
-  if (mongoose.connection.readyState >= 1) {
-    return;
-  }
   try {
-    if (!process.env.MONGO_URI) {
-      logger.error("MONGO_URI is missing in environment variables.");
-      return;
-    }
     await mongoose.connect(process.env.MONGO_URI);
     logger.info("MongoDB connected successfully");
   } catch (error) {
